c3b31f72e4350ffe554e53f92c93a4c56da4b95c
cryslei <crysleiferreiragomes1998@gmail.com>
2022-01-15 10:36:23 -0300

    refatoração: removendo comentarios

--- js/main.js
+++ js/main.js
@@ -1,31 +1,22 @@
 function convertUserName(username){
     size = username.length;
     if(size > 13){
-        var arrayName = username.split(' '); // separar cada palavra utilizando a funcao split
-        var nameReduce = arrayName[0]; //Adiciona o primeiro nome na variavel
-        arrayName.shift(); //remove a primeira posicao utilizando a funcao shift
-        // utiliza a funcao map para percorrer os elementos do array
+        let arrayName = username.split(' ');
+        let nameReduce = arrayName[0];
+        arrayName.shift();
         arrayName.map(function(element) 
         {
-            //verifica se as palavras reservadas esta contido no nome, e tambem se o nome é menor que 3 caracteres
-            // diminui o elemento para nao dar conflito ao procurar as palavra reservadas (da,do,das,dos,de)
             if(element.length <= 3){
-                //verifica o comprimento da variavel final
                 if(nameReduce.length < 13){
-                    //realiza a concatenacao dos dados
                     nameReduce += ' '+element;
                 }
             }else{
-                //verifica o comprimento da variavel final
                 if(nameReduce.length < 13){
-                    //realiza a concatenacao dos dados com a primeira letra pela funcao charAt
                     nameReduce += ' '+element.charAt(0)+'.';
                 }
             }
-        
         });
         return nameReduce;
-
     }else{
         return username;
     }    
